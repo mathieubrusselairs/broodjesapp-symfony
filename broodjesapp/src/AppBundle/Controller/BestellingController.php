b6e522ba565c039732beb7bfdcd3af6779af672b
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-03-26 14:23:53 +0200

    Initial setup BestellingController and DashboardController

--- broodjesapp/src/AppBundle/Controller/BestellingController.php
+++ broodjesapp/src/AppBundle/Controller/BestellingController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Bestelling;
 use AppBundle\Entity\Soep;
 use AppBundle\Entity\Supplement;
 use AppBundle\Entity\Brood;
+use AppBundle\Entity\Beleg;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 class BestellingController extends Controller
 {
     /**
-     * @Route("/", name="bestelling")
+     * @Route("/", name="bestelling_index")
      */
     public function indexAction()
     {
@@ -23,7 +24,7 @@ class BestellingController extends Controller
         $beleg = $em->getRepository('AppBundle:Beleg')->findAll();
         $soepvdag = $em->getRepository('AppBundle:Soep')->findBy(['id' => $today]);
         
-        return $this->render('default/index.html.twig', [
+        return $this->render('bestelling/index.html.twig', [
             'beleg' => $beleg,
             'soepvdag' => $soepvdag,
         ]);    
@@ -43,8 +44,7 @@ class BestellingController extends Controller
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($bestelling);
-            $this->entityManager->flush();
+            $this->BestellingService->persist($bestelling);
 
             return $this->redirectToRoute('/');
         }
@@ -54,4 +54,50 @@ class BestellingController extends Controller
             'form' => $form->createView(),
         ));
     }
+    
+    /**
+     * Displays a form to edit an existing Bestelling entity.
+     *
+     * @Route("/{id}/edit", name="bestelling_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Bestelling $bestelling)
+    {
+        $deleteForm = $this->createDeleteForm($bestelling);
+        $editForm = $this->createForm('AppBundle\Form\BestellingType', $bestelling);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('bestelling_edit', array('id' => $bestelling->getId()));
+        }
+
+        return $this->render('bestelling/edit.html.twig', array(
+            'bestelling' => $bestelling,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Bestelling entity.
+     *
+     * @Route("/{id}", name="bestelling_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Bestelling $bestelling)
+    {
+        $form = $this->createDeleteForm($bestelling);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($bestelling);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('bestelling_index');
+    }
+
 }
---
+++ broodjesapp/src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Bestelling;
+use AppBundle\Entity\Soep;
+use AppBundle\Entity\Supplement;
+use AppBundle\Entity\Brood;
+use AppBundle\Entity\Beleg;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends Controller
+{
+    
+}
