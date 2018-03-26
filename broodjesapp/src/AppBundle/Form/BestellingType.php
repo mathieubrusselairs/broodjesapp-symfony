574850d3fc480f7d000f24ba5e238db04f9b2616
Mathieu <mathieu.brusselairs@gmail.com>
2018-03-26 13:46:15 +0200

    Added bestellingstype for Symfony Form

---
+++ broodjesapp/src/AppBundle/Form/BestellingType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BestellingType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('beleg')->add('isWit')->add('isGroot')->add('beleg')->add('aantal')->add('opmerking');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Brood'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_brood';
+    }
+
+
+
+
+}
